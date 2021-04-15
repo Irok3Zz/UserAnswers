0e6c86c9285a0f035ad1f2872a6d339b020844c5
Irok3Zz <75535405+Irok3Zz@users.noreply.github.com>
2021-04-15 18:41:18 +0300

    Add files via upload

--- UserAnswers.cs
+++ UserAnswers.cs
@@ -17,13 +17,11 @@ namespace UserAnswers
         }
         public AnswerId SignUp(Sign newUser) // SignUp(Регистрация)
         {
-            StiksyDataBase DBquery = new StiksyDataBase();
             // Возвращает id нового User или -1, если такой пользователь уже занят
             return new AnswerId(DBquery.CreateUser(newUser.login, newUser.password), TypeId.IdUser);
         }
         public AnswerUser SignIn(Sign checkUser) // SignIn(Авторизация)
         {
-            StiksyDataBase DBquery = new StiksyDataBase();
             // Возвращает объект User или null, если нет совпадений в базе
             return new AnswerUser(DBquery.GetUserByLoginPassword(checkUser.login, checkUser.password));
         }
